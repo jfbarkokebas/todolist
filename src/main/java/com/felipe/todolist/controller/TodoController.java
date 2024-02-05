380512d156ebedb35f65ee0e2dd294b2a5ba965b
felipe <jfbarkokebas@gmail.com>
2024-02-05 18:44:39 -0300

    first version

---
+++ src/main/java/com/felipe/todolist/controller/TodoController.java
@@ -0,0 +1,48 @@
+package com.felipe.todolist.controller;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.felipe.todolist.entity.Todo;
+import com.felipe.todolist.service.TodoService;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    List<Todo>  create(Todo todo){
+       return todoService.create(todo);
+    }
+
+    @GetMapping
+    List<Todo> list(){
+        return todoService.list();
+    }
+    
+    @PutMapping
+    List<Todo> update(Todo todo){
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable("id") Long id){
+        return todoService.delete(id);
+    }
+    
+}
