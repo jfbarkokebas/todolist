99714369915515f3c95a03cf9b70325fc45f121e
felipe <jfbarkokebas@gmail.com>
2024-02-05 21:15:41 -0300

    fix post and update methods

--- src/main/java/com/felipe/todolist/controller/TodoController.java
+++ src/main/java/com/felipe/todolist/controller/TodoController.java
@@ -2,12 +2,14 @@ package com.felipe.todolist.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,14 +21,11 @@ import com.felipe.todolist.service.TodoService;
 @RequestMapping("/todos")
 public class TodoController {
 
+    @Autowired
     private TodoService todoService;
 
-    public TodoController(TodoService todoService) {
-        this.todoService = todoService;
-    }
-
     @PostMapping
-    List<Todo>  create(Todo todo){
+    List<Todo>  create(@RequestBody Todo todo){
        return todoService.create(todo);
     }
 
@@ -36,7 +35,7 @@ public class TodoController {
     }
     
     @PutMapping
-    List<Todo> update(Todo todo){
+    List<Todo> update(@RequestBody Todo todo){
         return todoService.update(todo);
     }
 
